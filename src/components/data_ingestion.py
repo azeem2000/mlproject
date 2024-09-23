2e7635a117caa2d9774b7362cd841fe1f771ce8a
unknown <azeemafraah@gmail.com>
2024-09-23 16:31:57 +0530

    data transformation and save object function added in utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -45,5 +47,10 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    try:
+        obj = DataIngestion()
+        train_data,test_data = obj.initiate_data_ingestion()
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_data,test_data)
+    except Exception as e:
+        raise CustomException(e,sys)
